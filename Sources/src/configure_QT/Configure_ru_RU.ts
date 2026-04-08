c46258425db63a3fa1c29e76cfaedb0ea8565bc9
Homer <homer-88@mail.ru>
2026-04-08 20:14:58 +0700

    add  configure
    configure add project

---
+++ Sources/src/configure_QT/Configure_ru_RU.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="Configure_ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="97"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="31"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="99"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;ф&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="34"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="106"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="47"/>
+        <location filename="mainwindow.ui" line="52"/>
+        <location filename="mainwindow.ui" line="57"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="101"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="102"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="103"/>
+        <source>Новый элемент</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="71"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="105"/>
+        <source>Laungage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="77"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="107"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="82"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="108"/>
+        <source>Страница</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="109"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="109"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="110"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="122"/>
+        <location filename="build-Configure-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/Configure_autogen/include/ui_mainwindow.h" line="111"/>
+        <source>ResetOptions</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
